af2520902f39dc7c93a2a4b50626212cf0af73c1
Ali Roohan <aliroohan321@gmail.com>
2025-03-13 21:24:02 +0500

    added environments

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const run = async () => {
-    await mongoose.connect("mongodb://localhost:27017/tag_heuer", {
+    await mongoose.connect(process.env.DB_CONFIG, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(() => {
